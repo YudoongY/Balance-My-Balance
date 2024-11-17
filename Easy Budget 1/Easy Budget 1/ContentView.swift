c0dbd8d007cd9b539051c662dd94e233cfda5406
Yudong <yudongyang111@gmail.com>
2024-11-16 22:43:30 -0800

    Data could be stored now! Problem fixed for the first step

--- Easy Budget 1/Easy Budget 1/ContentView.swift
+++ Easy Budget 1/Easy Budget 1/ContentView.swift
@@ -4,24 +4,13 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @State private var transactions: [Transaction]
+    @ObservedObject var account: Account
     @State private var showingAddTransactionSheet = false
-    @State private var accountName: String
     @State private var isAddingIncome = true // 用于判断添加的是收入还是支出
-    @State private var description: String
-    
-    var account: Account
-
-    init(account: Account) {
-        self.account = account
-        self.transactions = account.transactions
-        self.description = account.description
-        self.accountName = account.name
-    }
         
     var amount: Double {
-        transactions.filter { $0.isIncome }.map { $0.amount }.reduce(0, +)
-        - transactions.filter { !$0.isIncome }.map { $0.amount }.reduce(0, +)
+        account.transactions.filter { $0.isIncome }.map { $0.amount }.reduce(0, +)
+        - account.transactions.filter { !$0.isIncome }.map { $0.amount }.reduce(0, +)
     }
     
     var body: some View {
@@ -33,9 +22,9 @@ struct ContentView: View {
                         // Placeholder for account image (circle)
                         Circle()
                             .frame(width: 45, height: 45)
-                            .overlay(Text("A").font(.title))  // Placeholder with 'A'
+                            .overlay(Text(String(account.name.prefix(1))).font(.title))
                         
-                        Text(accountName)
+                        Text(account.name)
                             .font(.bold(.title)())
                         
                         Spacer()
@@ -49,11 +38,10 @@ struct ContentView: View {
                     .padding()
                     
                     VStack {
-                        Text(description)
-                        TextField("Description", text: $description)
-                            .onReceive(Just(description)) { newValue in
+                        TextField("Description", text: $account.description)
+                            .onReceive(Just(account.description)) { newValue in
                                 if newValue.count > 45 {
-                                    description = String(newValue.prefix(45))
+                                    account.description = String(newValue.prefix(45))
                                 }
                             } // Limit the number of char in the description line
                             .font(.headline)
@@ -125,9 +113,9 @@ struct ContentView: View {
             .sheet(isPresented: $showingAddTransactionSheet) {
                 VStack{
                     AddTransactionView(isIncome: isAddingIncome) { newTransaction in
-                        transactions.append(newTransaction)
+                        account.transactions.append(newTransaction)
                     }
-                    .presentationDetents([.height(250)]) // Limit the size of the window
+                    .presentationDetents([.height(250)]) // Adjust the size of the window
                 }// Pop up the sheet
             }
         }
@@ -135,7 +123,7 @@ struct ContentView: View {
     
     // 按日期分组交易记录
     private func groupTransactionsByDate() -> [TransactionDateGroup] {
-        let grouped = Dictionary(grouping: transactions) { $0.dateOnly }
+        let grouped = Dictionary(grouping: account.transactions) { $0.dateOnly }
         return grouped.map { TransactionDateGroup(date: $0.key, transactions: $0.value) }
             .sorted { $0.date > $1.date } // 按日期降序排列
     }
